dbaa6a818054ef2adf400a13ff1d8eea0816d910
Peter Pfeufer <ppfeufer@gmail.com>
2018-04-21 11:57:23 +0200

    comment template cleaned up Signed-off-by:Peter Pfeufer <ppfeufer@gmail.com>

--- comments.php
+++ comments.php
@@ -1,33 +1,43 @@
 <?php
-// DISPLAY COMMENTS IF COMMENTS ARE OPENED
 if(comments_open()) {
-	echo '<div class="comments">',
-	'<h2>';
-	_e('Comments', 'yulai-federation-wiki');
-	echo '</h2>';
-	if(have_comments()) {
+	?>
+	<div class="comments">',
+		<h2>
+			<?php _e('Comments', 'yulai-federation-wiki'); ?>
+		</h2>
+
+		<?php
 		// this is displayed if there are comments
-		echo '<h3>';
-		_e('This post currently has', 'yulai-federation-wiki');
-		echo '&nbsp;';
-		comments_number(__('no responses', 'yulai-federation-wiki'), __('one response', 'yulai-federation-wiki'), __('% responses', 'yulai-federation-wiki'));
-		echo '</h3>',
-		'<ul class="commentlist">';
-		wp_list_comments();
-		echo '</ul>',
-		'<div class="comment-nav">',
-		'<div class="alignleft">';
-		previous_comments_link();
-		echo '</div>',
-		'<div class="alignright">';
-		next_comments_link();
-		echo '</div>',
-		'</div>';
-	} else {
-		// this is displayed if there are no comments so far
-		_e('So empty here ... leave a comment!', 'yulai-federation-wiki');
-	}
-	// load comment form
-	comment_form();
-	echo '</div>'; // end of .content
+		if(have_comments()) {
+			?>
+			<h3>
+				<?php
+				_e('This post currently has', 'yulai-federation-wiki');
+				comments_number(__('no responses', 'yulai-federation-wiki'), __('one response', 'yulai-federation-wiki'), __('% responses', 'yulai-federation-wiki'));
+				?>
+			</h3>
+
+			<ul class="commentlist">
+				<?php wp_list_comments(); ?>
+			</ul>
+
+			<div class="comment-nav">
+				<div class="alignleft">';
+					previous_comments_link();
+				</div>
+				<div class="alignright">';
+					next_comments_link();
+				</div>
+			</div>
+			<?php
+		} else {
+			// this is displayed if there are no comments so far
+			_e('So empty here ... leave a comment!', 'yulai-federation-wiki');
+		}
+
+		// load comment form
+		comment_form();
+		?>
+	</div>
+	<?php
 }
