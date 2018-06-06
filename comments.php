2e8f6cba3dc04c7d5da9b70b1e986384aa278070
Peter Pfeufer <ppfeufer@gmail.com>
2018-06-06 10:18:09 +0200

    PHP functions now act as those again ... Signed-off-by:Peter Pfeufer <ppfeufer@gmail.com>

--- comments.php
+++ comments.php
@@ -1,43 +1,47 @@
 <?php
 if(comments_open()) {
-	?>
-	<div class="comments">
-		<h2>
-			<?php _e('Comments', 'yulai-federation-wiki'); ?>
-		</h2>
+    ?>
+    <div class="comments">
+        <h2>
+            <?php _e('Comments', 'yulai-federation-wiki'); ?>
+        </h2>
 
-		<?php
-		// this is displayed if there are comments
-		if(have_comments()) {
-			?>
-			<h3>
-				<?php
-				_e('This post currently has', 'yulai-federation-wiki');
-				comments_number(__('no responses', 'yulai-federation-wiki'), __('one response', 'yulai-federation-wiki'), __('% responses', 'yulai-federation-wiki'));
-				?>
-			</h3>
+        <?php
+        // this is displayed if there are comments
+        if(have_comments()) {
+            ?>
+            <h3>
+                <?php
+                _e('This post currently has', 'yulai-federation-wiki');
+                comments_number(__('no responses', 'yulai-federation-wiki'), __('one response', 'yulai-federation-wiki'), __('% responses', 'yulai-federation-wiki'));
+                ?>
+            </h3>
 
-			<ul class="commentlist">
-				<?php wp_list_comments(); ?>
-			</ul>
+            <ul class="commentlist">
+                <?php wp_list_comments(); ?>
+            </ul>
 
-			<div class="comment-nav">
-				<div class="alignleft">
-					previous_comments_link();
-				</div>
-				<div class="alignright">
-					next_comments_link();
-				</div>
-			</div>
-			<?php
-		} else {
-			// this is displayed if there are no comments so far
-			_e('So empty here ... leave a comment!', 'yulai-federation-wiki');
-		}
+            <div class="comment-nav">
+                <div class="alignleft">
+                    <?php
+                    previous_comments_link();
+                    ?>
+                </div>
+                <div class="alignright">
+                    <?php
+                    next_comments_link();
+                    ?>
+                </div>
+            </div>
+            <?php
+        } else {
+            // this is displayed if there are no comments so far
+            _e('So empty here ... leave a comment!', 'yulai-federation-wiki');
+        }
 
-		// load comment form
-		comment_form();
-		?>
-	</div>
-	<?php
+        // load comment form
+        comment_form();
+        ?>
+    </div>
+    <?php
 }
